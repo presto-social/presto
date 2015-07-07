07eb143aa29b60496c7b6c37a7c9c9d316c41bff
mkaptein172 <mkaptein172@icloud.com>
2015-07-07 21:51:41 +0200

    Added french

--- protected/modules_core/user/messages/fr/views_auth_login.php
+++ protected/modules_core/user/messages/fr/views_auth_login.php
@@ -1,9 +1,9 @@
 <?php
 return array (
   '<strong>Please</strong> sign in' => '<strong>Connexion</strong>',
-  '<strong>Sign</strong> up<br><br><ul><li>presto.social is free and always will be</li><li>We don’t sell your data</li><li>No App needed</li><li>Modern security standards</li></ul>' => '<strong>Inscription</strong>',
+  '<strong>Sign</strong> up<br><br><ul><li>presto.social is free and always will be</li><li>We don’t sell your data</li><li>No App needed</li><li>Modern security standards</li></ul>' => '<strong>Inscription</strong><br><br><ul><li>C’est gratuit (et ça le restera toujours)</li><li>Nous ne vendons pas vos informations</li><li>Pas App nécessaire </li><li>Normes de sécurité modernes</li></ul>',
   'Create a new one.' => 'Créer un nouveau.',
-  'Don\'t have an account? Join the network by entering your e-mail address. <a href="privacy_policy.html">Privacy Policy</a>' => 'Vous n\'avez pas de compte ? Rejoignez-nous en indiquant votre adresse e-mail.',
+  'Don\'t have an account? Join the network by entering your e-mail address. <a href="privacy_policy.html">Privacy Policy</a>' => 'Vous n\'avez pas de compte ? Rejoignez-nous en indiquant votre adresse e-mail. <a href="privacy_policy.html">Confidentialité</a>',
   'Forgot your password?' => 'Mot de passe perdu ?',
   'If you\'re already a member, please login with your username/email and password.' => 'Si vous êtes déjà inscrit, identifiez-vous à l\'aide de votre nom d\'utilisateur ou adresse e-mail ainsi que votre mot de passe.',
   'Register' => 'Enregistrez-vous',
